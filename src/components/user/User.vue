19f7c0abb4f63cef29e2833f8e9515917580da13
Nick Brown <nickrossbrown@gmail.com>
2018-12-11 20:53:38 -0800

    set up a home link in user route

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,23 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+    </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            navigateToHome() {
+
+// so this is the general theme of objects from vue itself or from your router in this case, having the
+// dollar sign at the beginning to mark that this is not our own object but instead is provided by
+// another package to us.
+                this.$router.push('/' );
+                // this.$router.push({ name: 'home' });
+                // this.$router.push({ path: '/' });
+            }
+        }
+    }
+</script>
