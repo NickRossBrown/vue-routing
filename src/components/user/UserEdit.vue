7d394fff60119c9c0fff5271ec8a1ed977d0d5aa
Nick Brown <nickrossbrown@gmail.com>
2018-12-11 21:49:55 -0800

    setup  and called query parameters

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,3 +1,8 @@
 <template>
-    <h3>Edit the User</h3>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Locale: {{ $route.query.local }}</p>
+        <p>Analytics: {{ $route.query.q }}</p>
+        <hr>
+    </div>
 </template>
