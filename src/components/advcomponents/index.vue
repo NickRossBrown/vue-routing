2ba297dcbc4a8f777907a173a7acd07b29cb830a
Nick Brown <nickrossbrown@gmail.com>
2018-12-12 00:35:37 -0800

    keeping component data alive. Selecting diffferents on the page

--- src/components/advcomponents/index.vue
+++ src/components/advcomponents/index.vue
@@ -4,6 +4,25 @@
             <div class="col-xs-12">
                 <p>Nick is Awesome!!!</p>
                 <p>Nick is Dope!!!</p>
+                
+                <button @click="selectedComponent = 'sapSaping'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>   
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <hr>
+                <app-new></app-new>
+                <app-author></app-author>
+                <hr>
                 <sap-saping>
                     <h2 slot="content">Nick is Awesome!!!</h2>
                     <h3 slot="title"> {{ quoteTitle }} </h3>
@@ -17,16 +36,20 @@
 
 <script>
     import Saying from './sayings/Saying.vue'
+    import Author from './sayings/Author.vue'
+    import New from './sayings/New.vue'
 
     export default {
         data: function() {
             return {
                 quoteTitle: 'The Quote',
-                selectedComponent: 'appQuote'
+                selectedComponent: 'sapSaping'
             }
         },
         components: {
-            sapSaping: Saying
+            sapSaping: Saying,
+            appAuthor: Author,
+            appNew: New
         }
         
     }
