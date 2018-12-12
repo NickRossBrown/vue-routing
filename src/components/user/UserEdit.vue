f5a4cb694172d449597a7f4c9ccbf37242ac2c49
Nick Brown <nickrossbrown@gmail.com>
2018-12-11 22:56:37 -0800

    passing a hash fragment and locating a page starting on that has.

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,5 +4,9 @@
         <p>Locale: {{ $route.query.local }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
         <hr>
+        <div style="height: 1800px"></div>
+        <div>
+            <p id="data"> Some Extra Data Stuff </p>
+        </div>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -7,7 +7,19 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    // the user navigated back with the back button, it's probably not the desired behavior to scroll him all
+    if (savedPosition) {
+      return savedPosition;
+    }
+    // navigates to the hash passed in the url
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    // default page location
+    return {x: 0, y: 0};
+  }
 })
 
 new Vue({
