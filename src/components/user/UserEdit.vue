b0ef5f51155da31196a87db548acfdc614838474
Nick Brown <nickrossbrown@gmail.com>
2018-12-11 23:10:55 -0800

    added page leave route check with user confirmation

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,9 +4,32 @@
         <p>Locale: {{ $route.query.local }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
         <hr>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
+        <hr>
         <div style="height: 1800px"></div>
         <div>
             <p id="data"> Some Extra Data Stuff </p>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+          return {
+              confirmed: false
+          }
+        },
+        beforeRouteLeave(to, from, next) {
+            if (this.confirmed) {
+                next();
+            } else {
+                if (confirm('Are you sure?')) {
+                    next();
+                } else {
+                    next(false);
+                }
+            }
+        }
+    }
+</script>
