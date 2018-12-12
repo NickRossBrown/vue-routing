f8617f9cfdfe97daf2e9f73f014575895a2a0518
Nick Brown <nickrossbrown@gmail.com>
2018-12-11 21:14:12 -0800

    hard coded user id into url and retreived user onto page

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,18 +2,21 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded Id: {{ id }}</p>
+        <hr>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
         methods: {
             navigateToHome() {
-
-// so this is the general theme of objects from vue itself or from your router in this case, having the
-// dollar sign at the beginning to mark that this is not our own object but instead is provided by
-// another package to us.
                 this.$router.push('/' );
                 // this.$router.push({ name: 'home' });
                 // this.$router.push({ path: '/' });
